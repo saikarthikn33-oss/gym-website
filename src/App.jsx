dea931c17710bcf514851c70ee26e30e9ed12975
Sai Karthik <your-saikarthikn33@gmail.com>
2026-04-13 15:12:06 +0530

    fix deployment issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import Navbar from './components/sections/Navbar';
-import Hero from './components/sections/Hero';
-import Highlights from './components/sections/Highlights';
-import MembershipPlans from './components/sections/MembershipPlans';
-import Booking from './components/sections/Booking';
-import Trainers from './components/sections/Trainers';
-import Gallery from './components/sections/Gallery';
-import Testimonials from './components/sections/Testimonials';
-import Contact from './components/sections/Contact';
-import CTASection from './components/sections/CTASection';
+import React from "react";
+import Navbar from "./components/sections/Navbar";
+import Hero from "./components/sections/Hero";
+import Highlights from "./components/sections/Highlights";
+import MembershipPlans from "./components/sections/MembershipPlans";
+import Booking from "./components/sections/Booking";
+import Trainers from "./components/sections/Trainers";
+import Gallery from "./components/sections/Gallery";
+import Testimonials from "./components/sections/Testimonials";
+import Contact from "./components/sections/Contact";
+import CTASection from "./components/sections/CTASection";
 
 function App() {
   return (
